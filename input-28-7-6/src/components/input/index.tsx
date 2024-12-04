1367b249fc387b0767391e979e266c560c5144ab
Michael <michaelslugin13@gmail.com>
2024-12-03 19:52:18 -0800

    Ready - handlers with types

--- input-28-7-6/src/components/input/index.tsx
+++ input-28-7-6/src/components/input/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent,  useState } from 'react';
+import { FunctionComponent, ChangeEvent, useState } from 'react';
 import style from './style.module.css';
 
 const UserInput:FunctionComponent = ():JSX.Element => {
@@ -6,22 +6,24 @@ const UserInput:FunctionComponent = ():JSX.Element => {
 const [adv,setAdv]=useState<string>(''), [text,setText]=useState<string>(''),
 typing ='Идёт набор текста...',    
 
-      hChange = (ev:any):void => {
-        const inputText =ev.target.value;   
+      hChange = (ev:ChangeEvent<HTMLInputElement>):void => {
+        const inputText = ev.target.value;   
         setText(inputText);
         if (inputText) {if (adv!==typing) {setAdv(typing);}} else {setAdv('');}
 },
-      hKeyDown = (ev:any):void => {
+      hKeyDown = (ev:React.KeyboardEvent<HTMLInputElement>):void => {
         if (ev.key==='Enter') {
                 if (text) {
                         setAdv('Вы послали вселенной: '+text);
-                        ev.target.value='';
+                        /* ev.target.value=''; if ev type is 'any' else setText & value in <input */
+                        setText('');
                 } else {setAdv('');}               
         }        
 };
   return (
 <div className={style.appContainer}> 
         <input  className={style.userInput1}
+                value={text}
                 placeholder='enter text here'
                 onChange={hChange}
                 onKeyDown= {hKeyDown}
